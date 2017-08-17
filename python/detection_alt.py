1a346671cf5c45d2663b81242d5c94b82a3c81a3
Alex <aldrake87@users.noreply.github.com>
2017-08-17 14:39:30 +0100

    Update detection_alt.py

--- python/detection_alt.py
+++ python/detection_alt.py
@@ -9,13 +9,16 @@ Created on Thu Jul 20 10:28:47 2017
 import cv2
 import numpy as np
 import time
-import uuid
 import logging
 import math
-#from urllib.request import urlopen
+import csv
 
 # Vehicle_counter from Dan Maesks response on 
 # https://stackoverflow.com/questions/36254452/counting-cars-opencv-python-issue/36274515#36274515
+
+# for testing
+tracked_blobs = []
+tracked_conts = []
 
 # ============================================================================
 
@@ -91,7 +94,8 @@ class VehicleCounter(object):
     @staticmethod
     def is_valid_vector(a):
         distance, angle = a
-        threshold_distance = max(10.0, -0.008 * angle**2 + 0.4 * angle + 25.0)
+        #threshold_distance = max(10.0, -0.008 * angle**2 + 0.4 * angle + 25.0)
+        threshold_distance = max(10.0, -0.001 * angle**2 + 25.0)
         return (distance <= threshold_distance)
 
 
@@ -99,8 +103,20 @@ class VehicleCounter(object):
         # Find if any of the matches fits this vehicle
         for i, match in enumerate(matches):
             contour, centroid = match
-
+            
+            # store the vehicle data
             vector = self.get_vector(vehicle.last_position, centroid)
+            b = dict(
+                    id = vehicle.id,
+                    center_x = centroid[0],
+                    center_y = centroid[1],
+                    vector_x = vector[0],
+                    vector_y = vector[1],
+                    counted = vehicle.counted,
+                    frame_number = frame_no
+                    )
+            tracked_blobs.append(b)
+            
             if self.is_valid_vector(vector):
                 vehicle.add_position(centroid)
                 self.log.debug("Added match (%d, %d) to vehicle #%d. vector=(%0.2f,%0.2f)"
@@ -164,8 +180,7 @@ class VehicleCounter(object):
 # Video source
 cap = cv2.VideoCapture('/Users/datascience9/Veh Detection/TFL API/625_201708101116.mp4')
 # Default background (as clear as possible or pre-created avg frame!!)
-#bg = "/Users/datascience9/Veh Detection/Sample Scripts/test_images/625frame207.jpg"
-bg = "/Users/datascience9/Veh Detection/Sample Scripts/test_images/default_bg.jpg"
+bg = "/Users/datascience9/Veh Detection/Sample Scripts/test_images/625_bg.jpg"
 
 # get frame size
 frame_w = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
@@ -173,7 +188,7 @@ frame_h = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
 
 # The cutoff for threshold. A lower number means smaller changes between
 # the average and current scene are more readily detected.
-THRESHOLD_SENSITIVITY = 30
+THRESHOLD_SENSITIVITY = 18
 # Blob size limit before we consider it for tracking.
 CONTOUR_WIDTH = 21
 CONTOUR_HEIGHT = 21
@@ -181,30 +196,33 @@ CONTOUR_HEIGHT = 21
 # here means that the average scene will pick up changes more readily,
 # thus making the difference between average and current scenes smaller.
 DEFAULT_AVERAGE_WEIGHT = 0.01
-INITIAL_AVERAGE_WEIGHT = DEFAULT_AVERAGE_WEIGHT * 50
+INITIAL_AVERAGE_WEIGHT = DEFAULT_AVERAGE_WEIGHT / 50
 # The number of seconds a blob is allowed to sit around without having
 # any new blobs matching it.
 BLOB_TRACK_TIMEOUT = 1.2 #0.7
 # Blob smoothing function, to join 'gaps' in cars
-SMOOTH = 6
+SMOOTH = int(round((CONTOUR_WIDTH**0.5)/2,0))
 # Constants for drawing on the frame.
 LINE_THICKNESS = 1
 
-#fourcc = cv2.VideoWriter_fourcc(*'mp4v')
-#out = cv2.VideoWriter('/Users/datascience9/Veh Detection/Outputs/625_201707221316w_output.mp4', fourcc, 20, (w, h))
-#outblob = cv2.VideoWriter('/Users/datascience9/Veh Detection/Outputs/625_201707221316w_outblob.mp4', fourcc, 20, (w, h))
+fourcc = cv2.VideoWriter_fourcc(*'mp4v')
+out = cv2.VideoWriter('/Users/datascience9/Veh Detection/Outputs/625_output.mp4', fourcc, 20, (frame_w, frame_h))
+outblob = cv2.VideoWriter('/Users/datascience9/Veh Detection/Outputs/625_outblob.mp4', fourcc, 20, (frame_w, frame_h))
+diffop = cv2.VideoWriter('/Users/datascience9/Veh Detection/Outputs/625_outdiff.mp4', fourcc, 20, (frame_w, frame_h))
 
-# A variable to store the running average.
-avg = None
 
-# create a baseline background average
-default_bg = cv2.imread(bg)
-default_bg = cv2.cvtColor(default_bg, cv2.COLOR_BGR2HSV)
-(_,_,default_bg) = cv2.split(default_bg)
-default_bg = cv2.GaussianBlur(default_bg, (21, 21), 0)
+if bg is None:
+    avg = None
+else:
+    # create a baseline background average
+    default_bg = cv2.imread(bg)
+    default_bg = cv2.cvtColor(default_bg, cv2.COLOR_BGR2HSV)
+    (_,_,default_bg) = cv2.split(default_bg)
+    avg = default_bg.copy().astype("float")
 
 # A list of "tracked blobs".
 blobs = []
+#tracked_blobs = []
 car_counter = None  # will be created later
 frame_no = 0
 
@@ -228,13 +246,13 @@ while(1):
         
         if avg is None:
             # Set up the average if this is the first time through.
-            #avg = grayFrame.copy().astype("float")
-            avg = default_bg.copy().astype("float")
+            avg = grayFrame.copy().astype("float")
+            #avg = default_bg.copy().astype("float")
             continue
         
         # Build the average scene image by accumulating this frame
         # with the existing average.
-        if frame_no == 1:
+        if frame_no < 10:
             def_wt = INITIAL_AVERAGE_WEIGHT
         else:
             def_wt = DEFAULT_AVERAGE_WEIGHT
@@ -244,13 +262,15 @@ while(1):
         # export averaged background for use in next video feed run
         if frame_no > 250:
             grayOp = cv2.cvtColor(cv2.convertScaleAbs(avg), cv2.COLOR_GRAY2BGR)
-            cv2.imwrite("/Users/datascience9/Veh Detection/Sample Scripts/test_images/default_bg.jpg",
+            cv2.imwrite("/Users/datascience9/Veh Detection/Sample Scripts/test_images/625_bg.jpg",
                         grayOp)
         
         # Compute the grayscale difference between the current grayscale frame and
         # the average of the scene.
         differenceFrame = cv2.absdiff(grayFrame, cv2.convertScaleAbs(avg))
         cv2.imshow("difference", differenceFrame)
+        diffout = cv2.cvtColor(differenceFrame, cv2.COLOR_GRAY2BGR)
+        diffop.write(diffout)
         
         # Apply a threshold to the difference: any pixel value above the sensitivity
         # value will be set to 255 and any pixel value below will be set to 0.
@@ -269,8 +289,8 @@ while(1):
         thresholdImage = cv2.dilate(opening, kernel, iterations = 2)
         
         cv2.imshow("threshold", thresholdImage)
-        #threshout = cv2.cvtColor(thresholdImage, cv2.COLOR_GRAY2BGR)
-        #outblob.write(threshout)
+        threshout = cv2.cvtColor(thresholdImage, cv2.COLOR_GRAY2BGR)
+        outblob.write(threshout)
 
         # Find contours aka blobs in the threshold image.
         _, contours, hierarchy = cv2.findContours(thresholdImage, 
@@ -293,26 +313,39 @@ while(1):
                 
                 center = (int(x + w/2), int(y + h/2))
                 blobs.append(((x, y, w, h), center))
-        
+                    
         for (i, match) in enumerate(blobs):
             contour, centroid = match
             x, y, w, h = contour
+            
+            # store the contour data
+            c = dict(
+                        frame_no = frame_no,
+                        centre_x = x,
+                        centre_y = y,
+                        width = w,
+                        height = h
+                        )
+            tracked_conts.append(c)
+            
             cv2.rectangle(frame, (x, y), (x + w - 1, y + h - 1), (0, 0, 255), LINE_THICKNESS)
             cv2.circle(frame, centroid, 2, (0, 0, 255), -1)
         
         if car_counter is None:
             print("Creating vehicle counter...")
-            car_counter = VehicleCounter(frame.shape[:2], frame.shape[0] / 2)
+            car_counter = VehicleCounter(frame.shape[:2], 2*frame.shape[0] / 3)
+            
         
         # draw dividing line
         cv2.line(frame, (0, int(2*frame_h/3)),(frame_w, int(2*frame_h/3)),
                  (0,0,255), LINE_THICKNESS)
-        
+
         car_counter.update_count(blobs, frame)
         
         # output video
         frame = cv2.cvtColor(frame, cv2.COLOR_HSV2BGR)
         cv2.imshow("preview", frame)
+        out.write(frame)
 
         if cv2.waitKey(27) & 0xFF == ord('q'):
                 break
@@ -322,4 +355,16 @@ while(1):
 cv2.line()
 cv2.destroyAllWindows()
 cap.release()
-#out.release()
+out.release()
+
+keys = tracked_blobs[0].keys()
+with open('tracked_blobs.csv', 'w') as output_file:
+    dict_writer = csv.DictWriter(output_file, keys)
+    dict_writer.writeheader()
+    dict_writer.writerows(tracked_blobs)
+    
+keys = tracked_conts[0].keys()
+with open('tracked_conts.csv', 'w') as output_file:
+    dict_writer = csv.DictWriter(output_file, keys)
+    dict_writer.writeheader()
+    dict_writer.writerows(tracked_conts)
